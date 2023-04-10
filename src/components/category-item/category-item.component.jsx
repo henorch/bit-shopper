eedd90cc9c8b29a7cbf7fbe7d9539e886b4509d3
henorch <henorch01@gmail.com>
2023-04-10 18:08:54 +0100

    creating category block

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,19 @@
+import './category-item.styles.scss'
+
+const Category = ({categories}) => {
+     const { title, imageUrl } = categories; 
+    return (
+        <div className='category-container'>
+            <div className='background-image' style={{
+                backgroundImage: `url(${imageUrl})`
+              }}/>
+              <div className='category-body-container'>
+                <h2>{title}</h2>
+                <p>SHOP NOW</p>
+              </div>
+            </div>
+            )
+          }
+    
+
+export default Category;
